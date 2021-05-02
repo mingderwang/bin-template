ce044fbedcc00eadaaab01ec5b828f4eaece0955
Ming-der Wang <>
2021-05-02 02:11:41 +0000

    for bin.js with minimist cli

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+
+'use strict'
 const a = require("@transmute/lds-ecdsa-secp256k1-recovery2020")
 console.log(a)
+
+
+const cryptoKeys = require('libp2p-crypto/src/keys')
+const argv = require('minimist')(process.argv.slice(2))
+
+async function main () {
+  console.log({
+    keyType: argv.type,
+    bits: argv.bits
+  })
+}
+
+main()
+
